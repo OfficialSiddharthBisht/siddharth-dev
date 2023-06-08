cbc30b7675aa5ac6b3cfb0a50b71c54ae4cf3284
Siddharth <officialsiddharthbisht@gmail.com>
2023-06-08 09:57:02 +0530

    feat: testimonials and SEO

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,5 +1,5 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "Completion of a developer course with distinction, mastering advanced programming concepts, software development methodologies, and cutting-edge technologies. Developed a diverse range of applications, demonstrating expertise in full-stack development, problem-solving, and collaboration. Recognized for delivering high-quality, efficient, and innovative solutions, contributing to successful project outcomes.",
     achievements : [
         {
             id : 1,
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -11,45 +11,45 @@ import boy3 from '../assets/svg/testimonials/boy3.svg'
 export const testimonialsData = [
     {
         id: 1,
-        name: 'Chandler Bing',
-        title: 'Transponster',
-        text: "Building my own portfolio could've never been easier than this. The only areas I'm good at are Math, Jokes and Dance.",
+        name: 'Himanshu Ahuja',
+        title: 'Founder Ahuja Products and Services',
+        text: "Siddharth Bisht's coding skills are exceptional! His attention to detail and ability to solve complex problems is truly impressive. He delivered outstanding results on our project and I highly recommend him as a developer.",
         image: boy2
     },
     {
         id: 2,
-        name: 'Monica Geller',
-        title: 'Head Chef',
-        text: "This is the only thing that I didn't have to spend time to sort out. It's tidy all by itself.",
+        name: 'Vishaka Aggrawal',
+        title: 'Godrej Enterprises',
+        text: "Siddharth Bisht's technical proficiency and innovative thinking have greatly impressed our team. He consistently delivered high-quality code and exceeded our expectations. His commitment to excellence is truly commendable. I wouldn't hesitate to recommend him to anyone seeking a skilled developer",
         image: girl2
     },
     {
         id: 3,
-        name: 'Phoebe Buffay',
-        title: 'Massuese',
-        text: "Heard of the song 'Smelly Cat!!' ? This website made it viral. Plus portfolio is the only thing that Ursula doesn't have :)",
-        image: girl1
-    },
-    {
-        id: 4,
-        name: 'Joey Tribbiani',
-        title: 'Actor',
-        text: "Making my portfolio with this helped me get my role as 'Dr. Drake Ramoray' in 'Days Of Our Lives'. You guys are the best !",
+        name: 'Mohd Kaif',
+        title: 'Founder After School Classes Nainital',
+        text: "Siddharth Bisht is an outstanding freelancer. His technical expertise, reliability, and ability to work independently make him a trusted partner. He consistently goes above and beyond to ensure project success. I highly recommend Siddharth for freelancing opportunities",
         image: boy1
     },
-    {
-        id: 5,
-        name: 'Rachel Green',
-        title: 'Sales Executive',
-        text: 'Ralph Lauren was one of the best things that ever happened to me. My portfolio made that dream happen. I owe you guys.',
-        image: girl3
-    },
-    {
-        id: 6,
-        name: 'Ross Geller',
-        title: 'Paleontologist',
-        text: "Be it 'Rocks' or 'Bones', you need a website to display it. This is it. Great work guys !",
-        image: boy3
-    }
+    // {
+    //     id: 4,
+    //     name: 'Joey Tribbiani',
+    //     title: 'Actor',
+    //     text: "Making my portfolio with this helped me get my role as 'Dr. Drake Ramoray' in 'Days Of Our Lives'. You guys are the best !",
+    //     image: boy1
+    // },
+    // {
+    //     id: 5,
+    //     name: 'Rachel Green',
+    //     title: 'Sales Executive',
+    //     text: 'Ralph Lauren was one of the best things that ever happened to me. My portfolio made that dream happen. I owe you guys.',
+    //     image: girl3
+    // },
+    // {
+    //     id: 6,
+    //     name: 'Ross Geller',
+    //     title: 'Paleontologist',
+    //     text: "Be it 'Rocks' or 'Bones', you need a website to display it. This is it. Great work guys !",
+    //     image: boy3
+    // }
 ]
 
